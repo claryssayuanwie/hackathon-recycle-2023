dba632c05570bcecf47cfc698803a9a357b6c9fb
BuildTools <20588554+nicknsy@users.noreply.github.com>
2023-04-29 14:50:37 -0700

    Finish identify page data

--- src/routes/identify/+page.ts
+++ src/routes/identify/+page.ts
@@ -150,7 +150,9 @@ export const load = (async () => {
                     {
                         image: seventyImg,
                         paragraphs: [
-                            
+                            `If you have a lot of scraps of glass of various colors, it is not easy for recycling workers to handle because broken glass is dangerous`,
+                            `You can create your own recycled glass mosaic. It is not as daunting of a task as it may seem, in fact it’s quite easy to make a beautiful artwork with mixed glass.`,
+                            `Glass shards may be sharp, so you may want to tumble your glass or collect shards off of beaches where they have been naturally tumbled to smooth out the edges.`,
                         ]
                     }
                 ]
@@ -163,7 +165,9 @@ export const load = (async () => {
                     {
                         image: seventyOneImg,
                         paragraphs: [
-                            
+                            `All glass is recycled in the same way. You will be asked to separate glass by color if you are selling it at a recycling facility. It is 100% recyclable because it doesn’t lose its original quality.`,
+                            `Here are some exceptions: Glass that has been treated with chemicals. Pyrex and ceramics have been treated with chemicals so that they can withstand high heat, thus they can’t be recycled. Drinking glasses also cannot be recycled because of chemical treatments used to raise the melting point. If your glass is frosted it is likely that it has been treated with chemicals and is no longer recyclable.`,
+                            `Broken glass is too dangerous to handle, so put it in a labeled cardboard box before recycling to improve the safety conditions at the recycling facility.`,
                         ]
                     }
                 ]
@@ -176,7 +180,8 @@ export const load = (async () => {
                     {
                         image: seventyTwoImg,
                         paragraphs: [
-                            
+                            `If you have an intact glass bottle, you can keep using it forever or even recycle it for money if it has not been chemically treated.`,
+                            `If you want to add some aesthetic for your garden, A wine bottle can be made into a potted plant if you remove the bottom of the bottle, tip it upside down and fasten it to a fence to fill with soil and your choice of plants.`,
                         ]
                     }
                 ]
@@ -190,7 +195,9 @@ export const load = (async () => {
                     {
                         image: twentyImg,
                         paragraphs: [
-                            
+                            `Cardboard is curbside recyclable. However you can reduce your waste by reusing cardboard boxes for storage, or creating 3D models, artwork, or paintings.`,
+                            `Whether they’re from a shipment or you’ve just moved and have all of those moving boxes left, cardboard boxes can pile up quickly and the only way to get rid of them is to break them down and take them to the recycling center, right?`,
+                            `Really, carboard boxes can easily be used as storage totes or saved for moving time.`
                         ]
                     }
                 ]
@@ -203,7 +210,9 @@ export const load = (async () => {
                     {
                         image: twentyOneImg,
                         paragraphs: [
-                            
+                            `Paper is made of 33% recycled paper, 33% trees and plants, and 33% wood scraps. It is 100% recyclable.`,
+                            `All paper is recycled the same way, just make sure it is free of contamination such as grease, food waste, paint or glue. Tissue paper and shredded paper are not recyclable.`,
+                            `If you have sensitive information on your paper, shred it and deliver it to a shredded paper company.`,
                         ]
                     }
                 ]
@@ -216,7 +225,9 @@ export const load = (async () => {
                     {
                         image: twentyTwoImg,
                         paragraphs: [
-                            
+                            `Magazines, newspapers, cardstock, origami paper, book pages, watercolor paper, construction paper, scrapbook paper, and printer paper are all recyclable! That is because paper is made of 33% recycled paper, 33% trees and plants, and 33% wood scraps.`,
+                            `All paper is recycled the same way, just make sure it is free of contamination such as grease, food waste, paint or glue.`,
+                            `Tissue paper and shredded paper are not recyclable, but there are eco-friendly ways to dispose of them.`,
                         ]
                     }
                 ]
